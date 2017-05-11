cbad76ef77304949c192787acff625c2e43e8250
mholliday6611 <mholliday6611@gmail.com>
2017-05-11 17:15:57 -0400

    first push, just starting out

---
+++ js/wavey.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById('signin');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function myFunction2() {
+    var x = document.getElementById('register');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
